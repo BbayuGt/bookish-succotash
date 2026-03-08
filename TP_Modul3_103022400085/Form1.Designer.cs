b6db9ab325194cf7ed31c6c3fc819e05ec6823d4
BbayuGt <49893593+BbayuGt@users.noreply.github.com>
2026-03-08 11:55:35 +0700

    Menambah GUI

--- TP_Modul3_103022400085/Form1.Designer.cs
+++ TP_Modul3_103022400085/Form1.Designer.cs
@@ -28,12 +28,189 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            output = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            buttonplus = new Button();
+            button11 = new Button();
+            buttonequal = new Button();
+            SuspendLayout();
+            // 
+            // output
+            // 
+            output.Font = new Font("Segoe UI", 50F);
+            output.Location = new Point(149, 51);
+            output.Margin = new Padding(2);
+            output.Name = "output";
+            output.Size = new Size(438, 147);
+            output.TabIndex = 0;
+            output.Text = "Label Output";
+            output.Click += label1_Click;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 15F);
+            button1.Location = new Point(178, 203);
+            button1.Name = "button1";
+            button1.Size = new Size(118, 111);
+            button1.TabIndex = 1;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 15F);
+            button2.Location = new Point(302, 203);
+            button2.Name = "button2";
+            button2.Size = new Size(118, 111);
+            button2.TabIndex = 2;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 15F);
+            button3.Location = new Point(426, 203);
+            button3.Name = "button3";
+            button3.Size = new Size(118, 111);
+            button3.TabIndex = 3;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI", 15F);
+            button4.Location = new Point(178, 320);
+            button4.Name = "button4";
+            button4.Size = new Size(118, 111);
+            button4.TabIndex = 4;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Segoe UI", 15F);
+            button5.Location = new Point(302, 320);
+            button5.Name = "button5";
+            button5.Size = new Size(118, 111);
+            button5.TabIndex = 5;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Font = new Font("Segoe UI", 15F);
+            button6.Location = new Point(426, 320);
+            button6.Name = "button6";
+            button6.Size = new Size(118, 111);
+            button6.TabIndex = 6;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Font = new Font("Segoe UI", 15F);
+            button7.Location = new Point(178, 437);
+            button7.Name = "button7";
+            button7.Size = new Size(118, 111);
+            button7.TabIndex = 7;
+            button7.Text = "7";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Font = new Font("Segoe UI", 15F);
+            button8.Location = new Point(302, 437);
+            button8.Name = "button8";
+            button8.Size = new Size(118, 111);
+            button8.TabIndex = 8;
+            button8.Text = "8";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Font = new Font("Segoe UI", 15F);
+            button9.Location = new Point(426, 437);
+            button9.Name = "button9";
+            button9.Size = new Size(118, 111);
+            button9.TabIndex = 9;
+            button9.Text = "9";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // buttonplus
+            // 
+            buttonplus.Font = new Font("Segoe UI", 15F);
+            buttonplus.Location = new Point(178, 554);
+            buttonplus.Name = "buttonplus";
+            buttonplus.Size = new Size(118, 111);
+            buttonplus.TabIndex = 10;
+            buttonplus.Text = "+";
+            buttonplus.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.Font = new Font("Segoe UI", 15F);
+            button11.Location = new Point(302, 554);
+            button11.Name = "button11";
+            button11.Size = new Size(118, 111);
+            button11.TabIndex = 11;
+            button11.Text = "0";
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // buttonequal
+            // 
+            buttonequal.Font = new Font("Segoe UI", 15F);
+            buttonequal.Location = new Point(426, 554);
+            buttonequal.Name = "buttonequal";
+            buttonequal.Size = new Size(118, 111);
+            buttonequal.TabIndex = 12;
+            buttonequal.Text = "=";
+            buttonequal.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(784, 761);
+            Controls.Add(buttonequal);
+            Controls.Add(button11);
+            Controls.Add(buttonplus);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(output);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Label output;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button buttonplus;
+        private Button button11;
+        private Button buttonequal;
     }
 }
--- TP_Modul3_103022400085/Form1.cs
+++ TP_Modul3_103022400085/Form1.cs
@@ -6,5 +6,10 @@ namespace TP_Modul3_103022400085
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
