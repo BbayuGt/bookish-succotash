6b2db0135d986a03a19172f3980077ca6b76e1fe
BbayuGt <49893593+BbayuGt@users.noreply.github.com>
2026-03-08 12:28:17 +0700

    Clean up + menambah penjelasan

--- TP_Modul3_103022400085/Form1.cs
+++ TP_Modul3_103022400085/Form1.cs
@@ -9,17 +9,20 @@ namespace TP_Modul3_103022400085
             InitializeComponent();
         }
 
+        // Fungsi ini hanya digunakan untuk refresh output text
         private void refreshOutput()
         {
             output.Text = text;
         }
 
+        // Fungsi ini secara dinamis menambahkan text dari button, jadi butuh satu fungsi saja untuk 10 button (0-10)
         private void numButton_Click(object sender, EventArgs e)
         {
             text += (sender as Button).Text;
             refreshOutput();
         }
 
+        // Fungsi ini untuk menghitung (+), menambahkan apa yang ada pada label ke total.
         private void buttonplus_Click(object sender, EventArgs e)
         {
             total += Convert.ToInt32(text);
@@ -27,6 +30,7 @@ namespace TP_Modul3_103022400085
             refreshOutput();
         }
 
+        // Untuk menghitung dan menampilkan total
         private void buttonequal_Click(object sender, EventArgs e)
         {
             total += Convert.ToInt32(text);
